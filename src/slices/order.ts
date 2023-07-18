37ebadd96692c01e6257a7e7d57369130d4dc7c3
junho0956 <junho0956@naver.com>
2023-07-18 23:57:47 +0900

    slice 기본틀 작성

---
+++ src/slices/order.ts
@@ -0,0 +1,12 @@
+import { createSlice } from "@reduxjs/toolkit";
+
+const initialState = {};
+
+const orderSlice = createSlice({
+  name: 'order',
+  initialState,
+  reducers: {},
+  extraReducers: {}
+})
+
+export default orderSlice;
---
+++ src/store/reducer.ts
@@ -0,0 +1,11 @@
+import { combineReducers } from "@reduxjs/toolkit";
+
+import userSlice from '../slices/user';
+
+const rootReducer = combineReducers({
+  user: userSlice.reducer,
+});
+
+export default rootReducer;
+
+export type RootState = ReturnType<typeof rootReducer>;
